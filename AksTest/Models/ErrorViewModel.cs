4c77c256a68f3a98aa2f9f3c20f75a3e6fbca5b8
Joel981 <75721103+Joel981@users.noreply.github.com>
2022-09-25 18:39:55 +0530

    Add project files.

---
+++ AksTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AksTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
